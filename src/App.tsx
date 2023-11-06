40ed8d6149bea3c8e62205fe3e07f11ab91932bb
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-07 00:13:19 +0100

    state string

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { useState } from "react";
+
 function App() {
+	const [firstName, setFirstName] = useState("Roger");
+
 	return (
 		<>
-			<h1 className="text-3xl mb-3 text-slate-800">Info Site</h1>
-			<p>Welcome to this site.</p>
+			<h1 className="text-3xl mb-3 text-slate-800">State and Events</h1>
+			<div className="bg-slate-400 p-4 rounded flex gap-2">
+				<button onClick={() => setFirstName('Andy')}>Change</button>
+				<p className="text-2xl">FirstName: {firstName}</p>
+			</div>
 		</>
 	);
 }
